4ccc27f04b82787b8815b6df28128f4d88d978ab
TheLitis <lovalentin08@gmail.com>
2026-08-23 09:08:19 +0300

    build(parity): pin finalized runtime dependencies

--- kairos_backtest/validation_campaign.py
+++ kairos_backtest/validation_campaign.py
@@ -41,7 +41,7 @@ FROZEN_STRATEGY = StrategyConfig(
     minimum_confidence=0.67,
 )
 FROZEN_QUANT_SHA = "c74b9853bd97597b2104b2d9c4bcd5b7c6cefb24"
-RUNTIME_QUANT_SHA = "8474a25bd0afa58f4182ea69aaa0af71c7a01643"
+RUNTIME_QUANT_SHA = "d71d34db569e9441842f1ef4d6c9cfd8628aaea0"
 FROZEN_QUANT_URL = "https://github.com/Kairos-cryptoAI/kairos-quant-scouts.git"
 INITIAL_EQUITY = 10_000.0
 ALLOCATION = 0.25
